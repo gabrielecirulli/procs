230b76b794f6ee1310d0f5e270de1d3776a7c46e
Gabriele Cirulli <gabrycir@gmail.com>
2013-09-08 20:01:44 +0200

    balls representing prcesses

--- assets/js/canvas.js
+++ assets/js/canvas.js
@@ -33,21 +33,25 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // World setup
     this.scale = 30;
-    this.boundaryThreshold = 1000;
 
     this.world = new b2World(new b2Vec2(0, 9.81), false); // World with vertical gravity to -10 and sleep not allowed
 
-    this.fixtureDefinition = new b2FixtureDef;
-    this.bodyDefinition = new b2BodyDef;
+    // var fixtureDefinition = new b2FixtureDef;
+    // var bodyDefinition = new b2BodyDef;
 
     // Set up and refresh canvas size (also draws walls)
     window.addEventListener("resize", this.resize.bind(this));
     this.resize();
 
+    // Distance in any direction after which an object is placed back at the top
+    this.boundaryThreshold = Math.max(this.canvas.width, this.canvas.height) * 3;
+
+    this.toDelete = [];
+
     // Debug draw
     var debugDraw = new b2DebugDraw;
     debugDraw.SetSprite(this.ctx);
-    debugDraw.SetDrawScale(20);
+    debugDraw.SetDrawScale(this.scale);
     debugDraw.SetFillAlpha(0.3);
     debugDraw.SetLineThickness(2.0);
     debugDraw.SetFlags(b2DebugDraw.e_shapeBit | b2DebugDraw.e_jointBit);
@@ -59,6 +63,12 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   BoxManager.prototype.update = function () {
+    // Apply a force to bodies that need to be deleted
+    this.toDelete.forEach(function (body) {
+      body.ApplyForce(new b2Vec2(0, -20), body.GetWorldCenter());
+    });
+
+    // Perform simulation step and draw
     this.world.Step(1 / 30, 10, 10); // Framerate, velocity iterations, position iterations
     this.world.DrawDebugData();
 
@@ -66,10 +76,12 @@ document.addEventListener("DOMContentLoaded", function () {
     while (body) {
       var position = body.GetPosition();
       if (position.x > (this.canvas.width + this.boundaryThreshold) / this.scale
-          || position.x < -this.boundaryThreshold
+          || position.x < -this.boundaryThreshold / this.scale
           || position.y > (this.canvas.height + this.boundaryThreshold) / this.scale
-          || position.y < -this.boundaryThreshold) {
-        this.world.DestroyBody(body);
+          || position.y < -this.boundaryThreshold / this.scale) {
+        // console.log("FUCK");
+        body.SetPosition(new b2Vec2(this.canvas.width / 2 / this.scale, 0));
+        // this.world.DestroyBody(body);
       }
       body = body.GetNext();
     }
@@ -84,38 +96,77 @@ document.addEventListener("DOMContentLoaded", function () {
     this.buildWalls();
   };
 
-  BoxManager.prototype.add = function () {
-    this.bodyDefinition.type = b2_dynamicBody;
-    this.fixtureDefinition.shape = new b2CircleShape(20 / this.scale);
-    this.bodyDefinition.position.x = (this.canvas.width / 2 + Math.random()) / this.scale;
-    this.bodyDefinition.position.y = this.canvas.height / 2 / this.scale;
-    this.world.CreateBody(this.bodyDefinition).CreateFixture(this.fixtureDefinition);
+  BoxManager.prototype.defaultFixture = function () {
+    var fixtureDefinition = new b2FixtureDef;
+    fixtureDefinition.density = 1.0;
+    fixtureDefinition.friction = 0.5;
+    fixtureDefinition.restitution = 0.2;
+
+    return fixtureDefinition;
   };
 
-  BoxManager.prototype.addWall = function (x, y, width, height) {
-    this.fixtureDefinition.density = 1.0;
-    this.fixtureDefinition.friction = 0.5;
-    this.fixtureDefinition.restitution = 0.2;
+  BoxManager.prototype.wallFixture = function () {
+    return this.defaultFixture();
+  };
 
-    this.bodyDefinition.type = b2_staticBody;
+  BoxManager.prototype.ballFixture = function () {
+    return this.defaultFixture();
+  };
 
-    this.bodyDefinition.position.x = x;
-    this.bodyDefinition.position.y = y;
+  BoxManager.prototype.addBall = function (radius) {
+    var bodyDefinition = new b2BodyDef;
 
-    var shape = this.wallShape(width, height);
-    // this.fixtureDefinition.shape = new b2PolygonShape;
-    // this.fixtureDefinition.shape.SetAsBox(width, height);
+    bodyDefinition.type = b2_dynamicBody;
+    bodyDefinition.position.x = ((this.canvas.width / 2) + (this.canvas.width / 1.2) * (Math.random() -.5)) / this.scale;
+    bodyDefinition.position.y = (-(this.canvas.height * 2) + (this.canvas.height * 2) * (Math.random() -.5)) / this.scale;
 
-    var body = this.world.CreateBody(this.bodyDefinition);
-    body.CreateFixture2(shape);
+    var fixtureDefinition = this.ballFixture();
+    fixtureDefinition.shape = new b2CircleShape(radius / this.scale);
+
+    var body = this.world.CreateBody(bodyDefinition);
+    body.CreateFixture(fixtureDefinition);
 
     return body;
+  };
+
+  BoxManager.prototype.adjustBallRadius = function (ball, radius) {
+    ball.DestroyFixture(ball.GetFixtureList());
+    var fixtureDefinition = this.ballFixture();
+    fixtureDefinition.shape = new b2CircleShape(radius / this.scale);
+
+    ball.CreateFixture(fixtureDefinition);
+  };
+
+  BoxManager.prototype.removeBall = function (ball) {
+    var self = this;
+    this.toDelete.push(ball);
+    // console.log("Pushed")
+    setTimeout(function () {
+      self.world.DestroyBody(self.toDelete.shift());
+    }, 10000);
   };
 
   BoxManager.prototype.wallShape = function (width, height) {
     var shape = new b2PolygonShape;
     shape.SetAsBox(width, height);
+
     return shape;
+  };
+
+  BoxManager.prototype.addWall = function (x, y, width, height) {
+    var bodyDefinition = new b2BodyDef;
+
+    bodyDefinition.type = b2_staticBody;
+    bodyDefinition.position.x = x;
+    bodyDefinition.position.y = y;
+
+    var fixtureDefinition = this.wallFixture();
+    fixtureDefinition.shape = this.wallShape(width, height);
+
+    var body = this.world.CreateBody(bodyDefinition);
+    body.CreateFixture(fixtureDefinition);
+
+    return body;
   };
 
   BoxManager.prototype.buildWalls = function () {
@@ -139,30 +190,28 @@ document.addEventListener("DOMContentLoaded", function () {
     } else {
       this.walls.bottom.SetPosition(bottomWallPosition);
       this.walls.bottom.DestroyFixture(this.walls.bottom.GetFixtureList());
-      this.walls.bottom.CreateFixture2(this.wallShape(bottomWallSize.x, bottomWallSize.y));
+      var bottomFixture = this.wallFixture();
+      bottomFixture.shape = this.wallShape(bottomWallSize.x, bottomWallSize.y);
+      this.walls.bottom.CreateFixture(bottomFixture);
 
       this.walls.right.SetPosition(rightWallPosition);
       this.walls.right.DestroyFixture(this.walls.right.GetFixtureList());
-      this.walls.right.CreateFixture2(this.wallShape(rightWallSize.x, rightWallSize.y));
+      var rightFixture = this.wallFixture();
+      rightFixture.shape = this.wallShape(rightWallSize.x, rightWallSize.y);
+      this.walls.right.CreateFixture(rightFixture);
 
       this.walls.left.SetPosition(leftWallPosition);
       this.walls.left.DestroyFixture(this.walls.left.GetFixtureList());
-      this.walls.left.CreateFixture2(this.wallShape(leftWallSize.x, leftWallSize.y));
+      var leftFixture = this.wallFixture();
+      leftFixture.shape = this.wallShape(leftWallSize.x, leftWallSize.y);
+      this.walls.left.CreateFixture(leftFixture);
     }
   };
 
   manager = new BoxManager;
 
-
-
-
-
-
-
-
-
-
-
   // Procs view functions
-  procs.view.add = manager.add.bind(manager);
+  procs.view.addBall = manager.addBall.bind(manager);
+  procs.view.adjustBallRadius = manager.adjustBallRadius.bind(manager);
+  procs.view.removeBall = manager.removeBall.bind(manager);
 });
--- assets/js/socket.js
+++ assets/js/socket.js
@@ -1,12 +1,62 @@
 document.addEventListener("DOMContentLoaded", function () {
   var procs = window.procs = window.procs || {};
 
+  var processes = {
+    // 19294: {
+    //   pid: 19294,
+    //   mem: 3488, // Bytes
+    //   name: "something",
+    //   ball: /* Box2D object */
+    // }
+  };
+
   // Socket handling
   var socket = io.connect();
 
-  socket.on("processes", function (processes) {
-    procs.view.add();
-    // console.log(processes.list[0]);
+  socket.on("processes", function (data) {
+    var list = data.list;
+    var newPids = Object.keys(list);
+    var oldPids = Object.keys(processes);
+
+    var orphans = oldPids.filter(function(pid) { return newPids.indexOf(pid) === -1 });
+
+
+    newPids.forEach(function (pid) {
+      // var orphanIndex = orphans.indexOf[pid];
+      // if (orphanIndex !== -1) orphans.pop(orphanIndex);
+
+      if (oldPids.indexOf(pid) === -1) { // If there's a process that wasn't there before
+        // Add the new process
+        processes[pid] = list[pid];
+        // console.log(pid);
+        processes[pid].ball = procs.view.addBall(sizeFromMem(processes[pid].mem));
+      } else {
+        // Check if the memory has changed
+        var diff = Math.abs(processes[pid].mem - list[pid].mem);
+        if(diff > 50000) {
+          // console.log(diff);
+          processes[pid].mem = list[pid].mem;
+          procs.view.adjustBallRadius(processes[pid].ball, sizeFromMem(processes[pid].mem));
+        }
+      }
+    });
+
+    // console.log(orphans);
+    orphans.forEach(function (pid) {
+      procs.view.removeBall(processes[pid].ball);
+      delete processes[pid];
+    });
   });
+
+
+
+  // Other functions
+  function log10(val) {
+    return Math.log(val) / Math.LN10;
+  }
+
+  function sizeFromMem(mem) {
+    return Math.pow(log10(mem), 3) / 15 + 2;
+  }
 });
 
