ac8d04eb24c70c388c169b9c7ef89aefe817ed9a
Gabriele Cirulli <gabrycir@gmail.com>
2013-09-08 16:59:25 +0200

    basic world

--- assets/js/canvas.js
+++ assets/js/canvas.js
@@ -14,21 +14,6 @@ document.addEventListener("DOMContentLoaded", function () {
             };
   })();
 
-  // Canvas
-  var canvas = document.getElementById("view");
-  var ctx = canvas.getContext("2d");
-
-  // Canvas management
-  window.addEventListener("resize", resize);
-
-  function resize() {
-    // Add b2world resizing here
-    canvas.width = document.documentElement.clientWidth;
-    canvas.height = document.documentElement.clientHeight;
-  }
-
-  resize();
-
   // Box2D variables
   var b2World = Box2D.Dynamics.b2World;
   var b2Vec2 = Box2D.Common.Math.b2Vec2;
@@ -40,49 +25,144 @@ document.addEventListener("DOMContentLoaded", function () {
   var b2CircleShape = Box2D.Collision.Shapes.b2CircleShape;
   var b2DebugDraw = Box2D.Dynamics.b2DebugDraw;
 
-  // World setup
-  var SCALE = 30;
+  // Box2D manager
+  function BoxManager() {
+    // Canvas
+    this.canvas = document.getElementById("view");
+    this.ctx = this.canvas.getContext("2d");
 
-  var world = new b2World(new b2Vec2(0, 9.81), true); // World with vertical gravity to -10 and sleep allowed
-  var fixtureDefinition = new b2FixtureDef;
-  fixtureDefinition.density = 1.0;
-  fixtureDefinition.friction = 0.5;
-  fixtureDefinition.restitution = 0.2;
+    // World setup
+    this.scale = 30;
+    this.boundaryThreshold = 1000;
 
-  var bodyDefinition = new b2BodyDef;
-  bodyDefinition.type = b2_staticBody;
+    this.world = new b2World(new b2Vec2(0, 9.81), false); // World with vertical gravity to -10 and sleep not allowed
 
-  bodyDefinition.position.x = canvas.width / 2 / SCALE;
-  bodyDefinition.position.y = canvas.height / SCALE;
+    this.fixtureDefinition = new b2FixtureDef;
+    this.bodyDefinition = new b2BodyDef;
 
-  fixtureDefinition.shape = new b2PolygonShape;
-  fixtureDefinition.shape.SetAsBox((canvas.width / SCALE) / 2, (10 / SCALE) / 2);
-  world.CreateBody(bodyDefinition).CreateFixture(fixtureDefinition);
+    // Set up and refresh canvas size (also draws walls)
+    window.addEventListener("resize", this.resize.bind(this));
+    this.resize();
 
-  // Debug draw
-  var debugDraw = new b2DebugDraw;
-  debugDraw.SetSprite(ctx);
-  debugDraw.SetDrawScale(SCALE);
-  debugDraw.SetFillAlpha(0.3);
-  debugDraw.SetLineThickness(2.0);
-  debugDraw.SetFlags(b2DebugDraw.e_shapeBit | b2DebugDraw.e_jointBit);
-  world.SetDebugDraw(debugDraw);
+    // Debug draw
+    var debugDraw = new b2DebugDraw;
+    debugDraw.SetSprite(this.ctx);
+    debugDraw.SetDrawScale(20);
+    debugDraw.SetFillAlpha(0.3);
+    debugDraw.SetLineThickness(2.0);
+    debugDraw.SetFlags(b2DebugDraw.e_shapeBit | b2DebugDraw.e_jointBit);
+    this.world.SetDebugDraw(debugDraw);
 
-  function update() {
-    world.Step(1 / 25, 10, 10); // Framerate, velocity iterations, position iterations
-    world.DrawDebugData();
-
-    requestAnimFrame(update);
+    // Run
+    this.update = this.update.bind(this);
+    requestAnimFrame(this.update);
   }
 
-  requestAnimFrame(update);
+  BoxManager.prototype.update = function () {
+    this.world.Step(1 / 30, 10, 10); // Framerate, velocity iterations, position iterations
+    this.world.DrawDebugData();
+
+    var body = this.world.GetBodyList();
+    while (body) {
+      var position = body.GetPosition();
+      if (position.x > (this.canvas.width + this.boundaryThreshold) / this.scale
+          || position.x < -this.boundaryThreshold
+          || position.y > (this.canvas.height + this.boundaryThreshold) / this.scale
+          || position.y < -this.boundaryThreshold) {
+        this.world.DestroyBody(body);
+      }
+      body = body.GetNext();
+    }
+
+    requestAnimFrame(this.update);
+  };
+
+  BoxManager.prototype.resize = function () {
+    this.canvas.width = document.documentElement.clientWidth;
+    this.canvas.height = document.documentElement.clientHeight;
+
+    this.buildWalls();
+  };
+
+  BoxManager.prototype.add = function () {
+    this.bodyDefinition.type = b2_dynamicBody;
+    this.fixtureDefinition.shape = new b2CircleShape(20 / this.scale);
+    this.bodyDefinition.position.x = (this.canvas.width / 2 + Math.random()) / this.scale;
+    this.bodyDefinition.position.y = this.canvas.height / 2 / this.scale;
+    this.world.CreateBody(this.bodyDefinition).CreateFixture(this.fixtureDefinition);
+  };
+
+  BoxManager.prototype.addWall = function (x, y, width, height) {
+    this.fixtureDefinition.density = 1.0;
+    this.fixtureDefinition.friction = 0.5;
+    this.fixtureDefinition.restitution = 0.2;
+
+    this.bodyDefinition.type = b2_staticBody;
+
+    this.bodyDefinition.position.x = x;
+    this.bodyDefinition.position.y = y;
+
+    var shape = this.wallShape(width, height);
+    // this.fixtureDefinition.shape = new b2PolygonShape;
+    // this.fixtureDefinition.shape.SetAsBox(width, height);
+
+    var body = this.world.CreateBody(this.bodyDefinition);
+    body.CreateFixture2(shape);
+
+    return body;
+  };
+
+  BoxManager.prototype.wallShape = function (width, height) {
+    var shape = new b2PolygonShape;
+    shape.SetAsBox(width, height);
+    return shape;
+  };
+
+  BoxManager.prototype.buildWalls = function () {
+    var wallThickness = 400 / 2;
+
+    var bottomWallPosition = new b2Vec2(this.canvas.width / 2 / this.scale, (this.canvas.height + wallThickness) / this.scale);
+    var bottomWallSize = new b2Vec2(this.canvas.width / 2 / this.scale, wallThickness / this.scale);
+
+    var rightWallPosition = new b2Vec2((this.canvas.width + wallThickness) / this.scale, (this.canvas.height / 2) / this.scale);
+    var rightWallSize = new b2Vec2(wallThickness / this.scale, this.canvas.height / 2 / this.scale);
+
+    var leftWallPosition = new b2Vec2((-wallThickness) / this.scale, (this.canvas.height / 2) / this.scale);
+    var leftWallSize = new b2Vec2(wallThickness / this.scale, this.canvas.height / 2 / this.scale);
+
+    if (!this.walls) {
+      this.walls = {};
+
+      this.walls.bottom = this.addWall(bottomWallPosition.x, bottomWallPosition.y, bottomWallSize.x, bottomWallSize.y);
+      this.walls.right = this.addWall(rightWallPosition.x, rightWallPosition.y, rightWallSize.x, rightWallSize.y);
+      this.walls.left = this.addWall(leftWallPosition.x, leftWallPosition.y, leftWallSize.x, leftWallSize.y);
+    } else {
+      this.walls.bottom.SetPosition(bottomWallPosition);
+      this.walls.bottom.DestroyFixture(this.walls.bottom.GetFixtureList());
+      this.walls.bottom.CreateFixture2(this.wallShape(bottomWallSize.x, bottomWallSize.y));
+
+      this.walls.right.SetPosition(rightWallPosition);
+      this.walls.right.DestroyFixture(this.walls.right.GetFixtureList());
+      this.walls.right.CreateFixture2(this.wallShape(rightWallSize.x, rightWallSize.y));
+
+      this.walls.left.SetPosition(leftWallPosition);
+      this.walls.left.DestroyFixture(this.walls.left.GetFixtureList());
+      this.walls.left.CreateFixture2(this.wallShape(leftWallSize.x, leftWallSize.y));
+    }
+  };
+
+  manager = new BoxManager;
+
+
+
+
+
+
+
+
+
+
 
   // Procs view functions
-  procs.view.add = function () {
-    bodyDefinition.type = b2_dynamicBody;
-    fixtureDefinition.shape = new b2CircleShape(20 / SCALE);
-    bodyDefinition.position.x = (canvas.width / 2 + Math.random()) / SCALE;
-    bodyDefinition.position.y = canvas.height / 2 / SCALE;
-    world.CreateBody(bodyDefinition).CreateFixture(fixtureDefinition);
-  }
+  procs.view.add = manager.add.bind(manager);
 });
